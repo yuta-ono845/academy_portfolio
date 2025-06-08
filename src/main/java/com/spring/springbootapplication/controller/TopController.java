cf44776c229cb2cc4b08f36d5f56871f8cb9912d
小野優太 <onoyuuta@onoyuutanoMacBook-Air.local>
2025-06-08 16:23:40 +0900

    項目削除機能実装

--- src/main/java/com/spring/springbootapplication/controller/TopController.java
+++ src/main/java/com/spring/springbootapplication/controller/TopController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import com.spring.springbootapplication.entity.User;
 import com.spring.springbootapplication.service.UserService;
+import com.spring.springbootapplication.service.TopChartService;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.spring.springbootapplication.security.LoginUser;
@@ -15,13 +17,23 @@ public class TopController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private TopChartService topChartService;
+
 
     @GetMapping("/top")
     public String showTopPage(Model model, @AuthenticationPrincipal LoginUser loginUser) {
-        User updatedUser = userService.findByEmail(loginUser.getUsername());
-        model.addAttribute("userName", updatedUser.getName());
-        model.addAttribute("bio", updatedUser.getBio());
-        model.addAttribute("profileImage", updatedUser.getProfileImage());
+        User user = userService.findByEmail(loginUser.getUsername());
+        Long userId = user.getId();
+
+        model.addAttribute("userName", user.getName());
+        model.addAttribute("bio", user.getBio());
+        model.addAttribute("profileImage", user.getProfileImage());
+
+        Map<String, Object> chartData = topChartService.buildChartData(userId);
+        model.addAttribute("chartLabels", chartData.get("chartLabels"));
+        model.addAttribute("chartDatasets", chartData.get("chartDatasets"));
+
         return "top";
 }
 }
